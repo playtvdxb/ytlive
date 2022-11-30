4098b452163b6c745765e4cdd667e52b99503ed6
Firoz Ahmed <firofame@gmail.com>
2022-11-30 22:24:09 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<h1 align="center"> Malayalam-IPTV </h1>
+
+[![M3U generator for YouTube](https://github.com/firofame/Malayalam-IPTV/actions/workflows/m3u_Generator.yml/badge.svg)](https://github.com/firofame/Malayalam-IPTV/actions/workflows/m3u_Generator.yml)
+
+`https://raw.githubusercontent.com/firofame/Malayalam-IPTV/main/youtube.m3u`
+
+Updated m3u links of YouTube live channels, **auto-updated every 3 hours**.
+
+### Usage
+
+Paste this URL: `https://raw.githubusercontent.com/firofame/Malayalam-IPTV/main/youtube.m3u` to any player which supports M3U playlists
+
+### Run the tool on your local machine
+
+```bash
+git clone https://github.com/firofame/Malayalam-IPTV.git
+cd Malayalam-IPTV
+chmod +x autorun.sh
+./autorun.sh
+```
+
+Do not forget to add a cron job set for every 4 hours(or 5) if you plan to run the script locally.
+
+Credits: benmoose39
---
+++ scripts/youtube_m3ugrabber.py
@@ -0,0 +1,51 @@
+#! /usr/bin/python3
+
+import requests
+import os
+import sys
+import json
+
+
+def grab(url):
+    response = requests.get(url, timeout=15).text
+    if '.m3u8' not in response:
+        #response = requests.get(url).text
+        if '.m3u8' not in response:
+            os.system(f'curl "{url}" > temp.txt')
+            response = ''.join(open('temp.txt').readlines())
+            if '.m3u8' not in response:
+                print(
+                    'https://raw.githubusercontent.com/firofame/Malayalam-IPTV/main/assets/moose_na.m3u')
+                return
+    end = response.find('.m3u8') + 5
+    tuner = 100
+    while True:
+        if 'https://' in response[end-tuner: end]:
+            link = response[end-tuner: end]
+            start = link.find('https://')
+            end = link.find('.m3u8') + 5
+            break
+        else:
+            tuner += 5
+    print(f"{link[start : end]}")
+
+
+print('#EXTM3U')
+
+f = open('../channel_list.json')
+data = json.load(f)
+for channel in data:
+    grp_title = channel['group-title']
+    tvg_logo = channel['tvg-logo']
+    tvg_id = channel['tvg-id']
+    ch_name = channel['ch_name']
+    print(
+        f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
+
+    url = channel['url']
+    grab(url)
+
+
+if 'temp.txt' in os.listdir():
+    os.system('rm temp.txt')
+    os.system('rm watch*')
